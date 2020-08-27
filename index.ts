de0969da6a49aaff3b9c5fbfb10ac3083c3f29f0
Kerdo <kerdokurs@gmail.com>
2020-08-27 20:14:16 +0300

    :sparkles: ruuterid, parameetrid

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import morgan from 'morgan';
+
 import { PrismaClient } from '@prisma/client';
 
 const app = express();
 const prisma = new PrismaClient();
+
+app.use(morgan('dev'));
 
 app.get('/', async (req, res) => {
   const maakonnad = await prisma.maakond.findMany({
@@ -26,6 +30,79 @@ app.get('/', async (req, res) => {
 
   res.json(maakonnad);
 });
+
+interface URLQuery {
+  fields?: string;
+  limit?: number;
+  order?: 'asc' | 'desc';
+}
+
+interface FetchParams {
+  select?: object;
+  take: number;
+  orderBy: object;
+}
+
+const routes: any = { maakonnad: 'maakond', linnad: 'linn' };
+
+const DEFAULT_LIMIT = 5;
+
+app.get('/get/:route', async (req, res) => {
+  try {
+    const { route } = req.params;
+
+    if (Object.keys(routes).indexOf(req.params.route) === -1)
+      throw new Error('Invalid route');
+
+    console.log(route);
+
+    const { fields, limit, order } = req.query as URLQuery;
+
+    const select: any = {};
+    for (let field of (fields || '').split(',')) select[field] = true;
+
+    const take =
+      limit && typeof limit === 'string' ? parseInt(limit) : DEFAULT_LIMIT;
+
+    const orderBy: any = {};
+
+    if (order) {
+      const splitted = order.split(',');
+      const orderIndex = splitted[0];
+      const orderWay = splitted[1];
+
+      orderBy[orderIndex] = orderWay;
+    } else {
+      orderBy['nimi'] = 'asc';
+    }
+
+    const options: FetchParams = { take, orderBy };
+
+    if (fields) options.select = select;
+
+    let data = await fetchData(prisma.maakond, options);
+
+    res.json({
+      limit: take,
+      amt: data.length,
+      data,
+      date: new Date(),
+    });
+  } catch (error) {
+    res.status(error.code || 400).json({ error: error.message });
+  }
+});
+
+async function fetchData(
+  instance: any,
+  { select, take, orderBy }: FetchParams
+) {
+  const options: any = { take, orderBy };
+
+  if (select) options.select = select;
+
+  return instance.findMany(options);
+}
 
 const port = process.env.PORT || 3000;
 
